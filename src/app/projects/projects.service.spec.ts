9b2f059e7c3b5259f8c51f3b312cd3e81fc4bb2f
Joice Fogaça <joi.fogaca@gmail.com>
2024-06-17 11:11:37 -0300

    Correção de testes

--- src/app/projects/projects.service.spec.ts
+++ src/app/projects/projects.service.spec.ts
@@ -1,12 +1,15 @@
 import { TestBed } from '@angular/core/testing';
 
 import { ProjectsService } from './projects.service';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 
 describe('ProjectsService', () => {
   let service: ProjectsService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [ provideHttpClient(withInterceptorsFromDi())]
+    });
     service = TestBed.inject(ProjectsService);
   });
 
--- src/app/projects/projects/projects.component.spec.ts
+++ src/app/projects/projects/projects.component.spec.ts
@@ -1,14 +1,32 @@
+import { FeaturedProject } from './../featuted-project/featured-projects.model';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ProjectsComponent } from './projects.component';
+import { ProjectsService } from '../projects.service';
+import { FeatutedProjectComponent } from '../featuted-project/featuted-project.component';
+import { SimpleProjectsComponent } from '../simple-projects/simple-projects.component';
+import { of } from 'rxjs';
 
 describe('ProjectsComponent', () => {
   let component: ProjectsComponent;
   let fixture: ComponentFixture<ProjectsComponent>;
-
+  let projectServiceSpy: jasmine.SpyObj<ProjectsService>;
   beforeEach(async () => {
+    projectServiceSpy = jasmine.createSpyObj<ProjectsService>('ProjectsService', {
+      getFeaturedProjects: of([]),
+      getSimpleProjects: of([]),
+    });
     await TestBed.configureTestingModule({
-      declarations: [ ProjectsComponent ]
+      imports: [
+
+      ],
+      providers: [
+        { provide: ProjectsService, useValue: projectServiceSpy }
+      ],
+      declarations: [ ProjectsComponent,
+        FeatutedProjectComponent,
+        SimpleProjectsComponent
+       ]
     })
     .compileComponents();
 
